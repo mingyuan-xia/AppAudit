7873686d4c19485ba020f3b831bcf25c91678237
Mingyuan Xia <ken.mingyuan@gmail.com>
2015-07-14 12:56:27 -0400

    bugfix

--- js/frontend.js
+++ js/frontend.js
@@ -2,7 +2,6 @@ function renderData(data, root, renderer) {
   root.empty();
   root.html(renderer(data));
   $('[data-toggle="tooltip"]').tooltip(); 
-  $('.rating').rating();  
   /*
   var ctx = $("#adsPercent").get(0).getContext("2d");
   var chart_data = data.file_dist;
